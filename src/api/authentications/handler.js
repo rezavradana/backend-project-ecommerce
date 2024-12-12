f796a05806c1f1d8430a8f544e303c3b7f0f7d18
IlhamAlifioD <alifiowijaya1424@gmail.com>
2024-12-12 11:49:24 +0700

    Modified authentication handler

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -19,34 +19,32 @@ class AuthenticationsHandler {
 		this._validator.validatePostAuthenticationPayload(request.payload);
 
 		const { email, password } = request.payload;
-
-          // ? Validasi Email dan Password
           const id = await this._usersService.verifyUserCredential(email, password);
-               if (!user) {
-                    return h.response({
-					status: "fail",
-					message: "Email atau Password salah!!",
-				}).code(400); 
-               }
 
-		// ? Membuat Token JWT
 		const accessToken = this._tokenManager.generateAccessToken({ id });
-               return {
-                    status: "success",
-                    message: "Login berhasil",
-                    data: {
-                         accessToken,
-                         refreshToken,
-                    },
-		};
+          const refreshToken = this._tokenManager.generateRefreshToken({ id });
+               await this._authenticationsService.addRefreshToken(refreshToken);
+
+          const response = h.response({
+               status: "success",
+               message: "Authentication berhasil ditambahkan",
+               data: {
+                    accessToken,
+                    refreshToken,
+               },
+          });
+     
+          response.code(201);
+          return response;
 	}
 
      async putAuthenticationHandler(request, h) {
+          this._validator.validatePutAuthenticationPayload(request.payload);
+          
           const { refreshToken } = request.payload;
-
-          await this._authenticationsService.verifyRefreshToken(refreshToken);
-
+               await this._authenticationsService.verifyRefreshToken(refreshToken);
           const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+
           const accessToken = this._tokenManager.generateAccessToken({ id });
                return h.response({
                     status: "success",
@@ -58,10 +56,10 @@ class AuthenticationsHandler {
      }
 
      async deleteAuthenticationHandler(request, h) {
+          this._validator.validateDeleteAuthenticationPayload(request.payload);
           const { refreshToken } = request.payload;
 
           await this._authenticationsService.deleteRefreshToken(refreshToken);
-
                return h.response({
                     status: "success",
                     message: "Refresh token dihapus",
